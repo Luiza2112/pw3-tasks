5f536a52b884561a6aee2bbdcc97c9da66d33d0b
Luiza2112 <ritafilhotes4@gmail.com>
2025-09-23 11:02:50 -0300

    :sparkles: Feat: Iniciar a implementação de segurança na API.

---
+++ pw3-tarefas/src/main/java/br/com/etechas/tarefas/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package br.com.etechas.tarefas.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration // O Spring reconhece a Classe como uma classe de configuração. É uma das primeiras a serem carregadas.
+@EnableWebSecurity
+public class SecurityConfig {
+    //Se usar construtor, não precisa do Bean
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults()); // Caso a API e a aplicação estejam rodando em portas diferentes, o Cors permite que ambos se comuniquem mesmo assim
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(
+                auth -> auth.anyRequest()
+                .authenticated());
+        http.sessionManagement( s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
