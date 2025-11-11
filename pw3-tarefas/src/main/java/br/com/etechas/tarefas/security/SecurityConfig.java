ebdb002ff012ff80b0bc32abcd2565f8a43e6a4f
Luiza2112 <ritafilhotes4@gmail.com>
2025-11-11 11:12:35 -0300

    :broom: Chore: Adicionar nome da dupla.

--- pw3-tarefas/src/main/java/br/com/etechas/tarefas/security/SecurityConfig.java
+++ pw3-tarefas/src/main/java/br/com/etechas/tarefas/security/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+// Luiza Gonçalves e Thabata Vitoria
 
 @Configuration // O Spring reconhece a Classe como uma classe de configuração. É uma das primeiras a serem carregadas.
 @EnableWebSecurity
